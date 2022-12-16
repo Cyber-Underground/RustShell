a02d18b6978d269c522f92ab0919c4086a747989
fides <jori.inberg@outlook.com>
2022-12-16 18:51:58 +0200

    Added some commands
    added a 'clear' and a 'files' command that needs some improving, the 'rm' command needs to skip files but it for some reason doesn't. The 'match' statement probably needs some "adjusting"

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
+#[allow(non_snake_case)]
+
+use std::io::{self, Write, stdout};
 use std::ffi::OsStr;
 use std::fs;
-use std::io::{self, Read, Write, stdout};
 use std::os::windows::ffi::OsStrExt;
-use std::process::{Command, Stdio};
 use winapi::um::wincon::SetConsoleTitleW;
 use colored::*;
-use std::env;
+use std::path::Path;
+use std::{time::Duration};
+
+// This function recursively prints the contents of a directory in a tree view-like format.
+// The `indent` parameter controls the amount of indentation for each level of the tree.
+fn print_directory_tree(path: &Path, indent: usize) {
+    // Print the name of the current directory, indented by the specified amount
+    println!("{:indent$}{}", "", path.to_string_lossy(), indent = indent * 4);
+
+    // Exit the function if the current indent level is greater than or equal to 2
+    // Change the indent value to make it search further
+    
+    if indent >= 3 {
+        return;
+    }
+    
+    // Get an iterator over the entries in the directory
+    let entries = fs::read_dir(path).unwrap();
+
+    // Iterate over the entries in the directory
+    for entry in entries {
+        // Get the path of the entry
+        let entry_path = entry.unwrap().path();
+
+        // If the entry is a directory, recursively print its contents
+        if entry_path.is_dir() {
+            print_directory_tree(&entry_path, indent + 0);
+        }
+        // Otherwise, just print the name of the entry
+        else {
+            println!("{:indent$}{}", "", entry_path.display(), indent = (indent + 1) * 4);
+        }
+    }
+}
 
 fn main() -> io::Result<()> {
+    ansi_term::enable_ansi_support().unwrap();
+
     // Clear the command prompt
     print!("{}[2J", 27 as char);
     stdout().flush().unwrap();
@@ -18,12 +54,13 @@ fn main() -> io::Result<()> {
         .encode_wide()
         .chain(Some(0).into_iter())
         .collect();
-    unsafe { SetConsoleTitleW(title.as_ptr()) };
+    unsafe { 
+        SetConsoleTitleW(title.as_ptr()) 
+    };
 
-    io::stdout().flush().unwrap();
-
-    // Print the lines
     let lines = [
+        "",
+        "",
         "     /$$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$ /$$       /$$      ",
         "    | $$__  $$ /$$__  $$| $$  | $$| $$_____/| $$      | $$      ",
         "    | $$  \\ $$| $$  \\__/| $$  | $$| $$      | $$      | $$      ",
@@ -35,125 +72,136 @@ fn main() -> io::Result<()> {
         "",
     ];
 
+    let access_denied = [
+        "    /$$$$$$                                                                         /$$    ",
+        "   |_  $$_/                                                                        | $$    ",
+        "     | $$   /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$  ",
+        "     | $$  | $$__  $$ /$$_____/ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$_____/|_  $$_/  ",
+        "     | $$  | $$  \\ $$| $$      | $$  \\ $$| $$  \\__/| $$  \\__/| $$$$$$$$| $$        | $$    ",
+        "     | $$  | $$  | $$| $$      | $$  | $$| $$      | $$      | $$_____/| $$        | $$ /$$",
+        "    /$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/| $$      | $$      |  $$$$$$$|  $$$$$$$  |  $$$$/",
+        "   |______/|__/  |__/ \\_______/ \\______/ |__/      |__/       \\_______/ \\_______/   \\___/  ",
+        "",
+    ];
+
+    let access_granted = [
+        "     /$$$$$$                                                      /$$    ",
+        "    /$$__  $$                                                    | $$    ",
+        "   | $$  \\__/  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$  ",
+        "   | $$       /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$_____/|_  $$_/  ",
+        "   | $$      | $$  \\ $$| $$  \\__/| $$  \\__/| $$$$$$$$| $$        | $$    ",
+        "   | $$    $$| $$  | $$| $$      | $$      | $$_____/| $$        | $$ /$$",
+        "   |  $$$$$$/|  $$$$$$/| $$      | $$      |  $$$$$$$|  $$$$$$$  |  $$$$/",
+        "    \\______/  \\______/ |__/      |__/       \\_______/ \\_______/   \\___/  ",
+        "",
+    ];
+
     for line in &lines {
-        println!("{}", line.bright_magenta());
+        println!("{}", line.truecolor(80, 0, 255));
     }
 
-    print!("    rshell> ");
+    let password = "1337";
+
+    print!("        Please enter the password: ");
     io::stdout().flush()?;
 
-/*
+    let mut user_input = String::new();
+    std::io::stdin()
+        .read_line(&mut user_input)
+        .expect("        Failed to read line");
+
+    if user_input.trim() == password {
+        print!("{}[2J", 27 as char);
+        stdout().flush().unwrap();
+        for line in &access_granted {
+            println!("{}", line.truecolor(57, 255, 20));
+        }
+        std::thread::sleep(Duration::new(1, 0));
+        print!("{}[2J", 27 as char);
+        stdout().flush().unwrap();
+        for line in &lines {
+            println!("{}", line.truecolor(80, 0, 255));
+        }
+    } else {
+        print!("{}[2J", 27 as char);
+        stdout().flush().unwrap();
+        for line in &access_denied {
+            println!("{}", line.truecolor(255, 0, 80));
+        }
+        std::thread::sleep(Duration::new(2, 0));
+        std::process::exit(0);
+    }
+
     loop {
-        // Read a line of input from the user
+        print!("    rshell > ");
+        io::stdout().flush()?;
+
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
 
-        // Split the input into words (i.e. the command and its arguments)
-        let words: Vec<&str> = input.split_whitespace().collect();
+        let input = input.trim();
 
-        if words.is_empty() {
+        let mut parts = input.split_whitespace();
+        let command = parts.next().unwrap_or("");
+
+        if input.is_empty() {
             // If the user didn't enter any input, print the prompt again and continue
-            print!("    rshell> ");
             io::stdout().flush()?;
             continue;
         }
-
-        // Get the command and its arguments
-        let command = words[0];
-        let args = &words[1..];
-
-        if command == "rm" {
-    // If the user entered the "rm" command, delete the specified file or directory
-    if args.len() != 1 {
-        println!("rm: missing operand");
-        print!("    rshell> ");
-        continue;
-    } else {
-        let path = args[0];
-        match fs::metadata(path) {
-            Ok(metadata) => {
-                if metadata.is_file() {
-                    match fs::remove_file(path) {
-                        Ok(_) => println!("rm: removed '{}'", path),
-                        Err(err) => {
-                            // Handle the error here
-                            println!("rm: failed to remove '{}': {}", path, err);
-                            // Continue execution
-                            continue;
-                        }
+        
+        match command {
+            "exit" => break,
+            "rm" | "remove" => {
+                // Get the first argument after the `rm` command, which should be the path to the file or directory to be deleted
+                let arg = parts.next().unwrap_or("");
+                match fs::remove_dir_all(arg) {
+                    // If the directory or file was successfully deleted, move on to the next one
+                    Ok(_) => continue,
+                    // If the error is that the path does not exist, print an error message and move on to the next one
+                    Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                        println!("        rm: {}: No such file or directory", arg);
+                        continue;
                     }
-                } else if metadata.is_dir() {
-                    match fs::remove_dir_all(path) {
-                        Ok(_) => println!("rm: removed '{}'", path),
-                        Err(err) => {
-                            // Handle the error here
-                            println!("rm: failed to remove '{}': {}", path, err);
-                            // Continue execution
-                            continue;
-                        }
+                    // If the error is that the file cannot be deleted, print an error message and move on to the next one
+                    Err(ref e) if e.kind() == io::ErrorKind::PermissionDenied => {
+                        println!("        rm: {}: Cannot delete file: permission denied", arg);
+                        continue;
+                    }
+                    // If the error is something other than the path not existing or permission denied, print the error and move on to the next one
+                    Err(e) => {
+                        eprintln!("        rm: {}: {}", arg, e);
+                        continue;
                     }
                 }
-            },
-            Err(err) => println!("rm: failed to get metadata for '{}': {}", path, err),
+            }
+            "files" => {
+                // Get the directory and indent level from the command line arguments
+                let mut args = std::env::args_os();
+
+                let directory = std::env::args().nth(1).unwrap_or_else(|| ".".to_string());
+
+
+                let indent_level = if args.len() > 2 {
+                    args.nth(2).unwrap().to_string_lossy().parse::<usize>().unwrap()
+                } else {
+                    0
+                };
+
+                // Call the print_directory_tree function to print the contents of the directory
+                print_directory_tree(Path::new(&directory), indent_level);
+            }
+            "clear" | "cls" => {
+                print!("{}[2J", 27 as char);
+                stdout().flush().unwrap();
+                for line in &lines {
+                    println!("{}", line.truecolor(80, 0, 255));
+                }
+            }
+            _ => println!("        Unknown command"),
         }
+
     }
-    io::stdout().flush()?;
-    continue;
-} else if command == "pwsh" {
-            // If the user entered the "pwsh" command, run the specified command in PowerShell
-            let mut child = Command::new("powershell.exe")
-                .args(args)
-                .stdin(Stdio::inherit())
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .spawn()?;
 
-            // Wait for the command to finish executing
-            let _result = child.wait()?;
-            continue;
-        } else if command == "elevate" {
-            // Create a new process that runs the "runas" command
-            let mut child = Command::new("runas")
-                .arg("/user:Administrator")
-                .arg(std::env::current_exe()?)
-                .args(args)
-                .stdin(Stdio::inherit())
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .spawn()?;
-
-            // Wait for the program to finish executing
-            let _result = child.wait()?;
-        } else if command == "connect" || command == "attach" {
-            // Create a new process that runs the specified program
-            let mut child = Command::new(args[0])
-                .args(&args[1..])
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .stderr(Stdio::inherit())
-                .spawn()?;
-
-            // Connect the standard input and output of the current program to the other program
-            let stdin = child.stdin.as_mut().unwrap();
-            let stdout = child.stdout.as_mut().unwrap();
-
-            // Read input from the user and write it to the other program
-            let mut input = String::new();
-            io::stdin().read_line(&mut input)?;
-            stdin.write_all(input.as_bytes())?;
-            stdin.flush()?;
-
-            // Read output from the other program and write it to the console
-            let mut output = String::new();
-            stdout.read_to_string(&mut output)?;
-            println!("{}", output);
-        } else {
-            println!("  rshell: command not found: {}", command);
-            print!("    rshell> ");
-            io::stdout().flush()?;
-            continue;
-        }
-    }
-*/
     Ok(())
 }
